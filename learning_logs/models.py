4be1bccb859b9fc9acad57c66a65c5759653280c
asraful01 <asraful.babul53@qmail.cuny.edu>
2020-08-29 21:17:46 -0400

    base page added, topic url and page added with entries

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -2,12 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Topic(models.Model):
-    """A topic the User is learning aobut"""
+    """A topic the user is learning about."""
     text = models.CharField(max_length=200)
-    date_added = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        verbose_name_plural="Topics"
+    date_added = models.DateTimeField(auto_now_add=True) 
 
     def __str__(self):
         """Return a string representation of the model."""
@@ -21,7 +18,7 @@ class Entry(models.Model):
     
     class Meta:
         verbose_name_plural = 'entries'
-
+ 
     def __str__(self):
         """Return a string representation of the model."""
-        return self.text[:50] + ("..." if len(self.text) > 50 else "")
+        return f"{self.text[:50]}..."
--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,10 +1,15 @@
-"""define URL patterns for learning_logs"""
+"""Defines URL patterns for learning_logs."""
+
 from django.urls import path
+
 from . import views
 
-app_name ="learning_logs"
-urlpatterns=[
-    #home page
-    path('',views.index, name='index'),
-    
+app_name = 'learning_logs'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+    # Page that shows all topics.
+    path('topics/', views.topics, name='topics'),
+    # Detail page for a single topic.
+   path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
