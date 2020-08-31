84738b693728940b21044b892ee9cb62a305a69e
asraful01 <asraful.babul53@qmail.cuny.edu>
2020-08-30 21:01:15 -0400

    adding new entries for particular topic form added

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
    path('topics/<int:topic_id>/', views.topic, name='topic'),
    #pAGE FOR ADDING new topic
    path("new_topic/", views.new_topic,name='new_topic'),
+   #page for adding new entry
+   path("new_entry/<int:topic_id>/",views.new_entry,name='new_entry'),
 ]
