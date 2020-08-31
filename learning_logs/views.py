6eb6bdd7b1659fdc95046f0d4ce9693747538943
asraful01 <asraful.babul53@qmail.cuny.edu>
2020-08-31 18:12:10 -0400

    bug fix

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -36,7 +36,7 @@ def new_topic(request):
         # POST data submitted; process data.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            new_topic= form.save(commit=false)
+            new_topic= form.save(commit=False)
             new_topic.owner=request.user
             new_topic.save()
             return redirect('learning_logs:topics')
