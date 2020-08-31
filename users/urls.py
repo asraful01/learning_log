bc6b79cb6d6e0032a5781e68baa0f8fd3936e487
asraful01 <asraful.babul53@qmail.cuny.edu>
2020-08-31 15:22:31 -0400

    login authentication created

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+"""defines URL patterns for users"""
+from django.urls import path, include
+
+app_name =  'users'
+urlpatterns=[
+    #include default auth urls
+    path('',include('django.contrib.auth.urls')),
+]
